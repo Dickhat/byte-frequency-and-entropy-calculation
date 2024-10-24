b66b67932814f17479754c25b54be5f05220e6ab
user <user@DESKTOP-1AMA56P>
2024-10-24 20:39:42 +0300

    Добавьте файлы проекта.

---
+++ byte_symbol_frequency_checker.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+// Второй параметр путь графического файла, третий параметр путь к отчету
+int main(int argc, char* argv[])
+{
+    ifstream file;      // Исходный файл
+    ofstream statistic; // Файл сводка
+    
+    file.open(argv[1], ios::binary);    // Дескриптор графического файла
+    statistic.open(argv[2], ios::out);  // Дескриптор статистического файла
+
+    int symbols[256];
+    int countOfSymbols = 0;
+
+    if (file.is_open() && statistic.is_open())
+    {
+        cout << "File is opened";
+
+        // Зануление массива
+        for (int i = 0; i < 256; ++i)
+            symbols[i] = 0;
+
+        char symbol;
+
+        // Подсчитывание байтов
+        while (file.read(&symbol, sizeof(byte)))
+        {
+            countOfSymbols++;
+            int temp = static_cast<int>(symbol);
+            symbols[int(symbol) + 128] += 1;
+        }
+
+        float entropy = 0;
+
+        // Вывод Частотного анализа и энтропии
+        for (int i = 0; i < 256; ++i)
+        {
+            float probability = float(symbols[i]) / countOfSymbols;
+            float partOfEntropy = probability * log2(probability);
+            statistic << left << setw(4) << i + 1 << " " << setw(5) << symbols[i] << " " 
+                      << left << setw(11) << probability << " " << left << setw(11) << partOfEntropy << endl;
+
+            entropy -= partOfEntropy;
+        }
+
+        statistic << setw(10) << countOfSymbols << " " << left << setw(11) << 1 << " " << left << setw(11) << entropy << endl;
+
+    }
+    else
+    {
+        cout << "File not opened";
+        return -1;
+    }
+
+    return 0;
+}
